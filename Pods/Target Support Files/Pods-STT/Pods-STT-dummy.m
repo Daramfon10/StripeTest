8f60ff3e3624468ad9a9916b2fe6cbb2845341f9
daramfon <Daramfon@buffalo.edu>
2019-11-02 22:01:36 -0400

    new stuff

---
+++ Pods/Target Support Files/Pods-STT/Pods-STT-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_STT : NSObject
+@end
+@implementation PodsDummy_Pods_STT
+@end
---
+++ STT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  STT
+//
+//  Created by Daramfon Akpan on 11/2/19.
+//  Copyright © 2019 Daramfon Akpan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
